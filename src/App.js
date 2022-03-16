a086a899d0fe2de877dd94a644bfc599d4ff7db6
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-03-15 17:59:08 -0700

    added counter to imgs and capacity to click img with some added css via bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-
 import "./App.css";
 import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
+import data from "./data.json";
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <>
-      <Header/>
-      <Main/>
-      <Footer/>
+        <Header />
+        <Main
+          data={data} 
+        />
+        <Footer />
       </>
     )
   }
